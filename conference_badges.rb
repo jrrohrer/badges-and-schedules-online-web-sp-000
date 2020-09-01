eb295bafeb702495de210c656284e15c222b9573
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-01 16:28:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(array)
   badges = []
   array.each do |name|
     badge_msg = "Hello, my name is #{name}."
